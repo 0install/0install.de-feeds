2465421fbbb985842e360094a5c9ed26b88e6696
Bastian Eicher <bastian@eicher.net>
2017-09-06 13:18:59 +0200

    Added watch file for FileZilla

---
+++ FileZilla.watch.py
@@ -0,0 +1,6 @@
+from urllib import request
+import re
+
+data = request.urlopen('https://sourceforge.net/projects/filezilla/files/FileZilla_Client/').read()
+matches = re.findall(r'([0-9\.]+)</a></th>\n\s*<td headers="files_date_h" class="opt"><abbr title="(....-..-..)', data.decode())
+releases = [{'version': match[0], 'released': match[1]} for match in matches]
