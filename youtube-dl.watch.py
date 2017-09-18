4ccfe40605c021fd91b96afb0b6b8722360a6b92
Bastian Eicher <bastian@eicher.net>
2017-09-18 18:01:34 +0200

    Updated youtube-dl

--- youtube-dl.watch.py
+++ youtube-dl.watch.py
@@ -2,4 +2,4 @@ from urllib import request
 import json
 
 data = request.urlopen('https://api.github.com/repos/rg3/youtube-dl/releases').read()
-releases = [{'version': release['tag_name'], 'released': release['published_at'][0:10]} for release in json.loads(data) if len(release['assets']) > 0]
+releases = [{'version': release['tag_name'].replace('.0', '.'), 'version-original': release['tag_name'], 'released': release['published_at'][0:10]} for release in json.loads(data) if len(release['assets']) > 0]
