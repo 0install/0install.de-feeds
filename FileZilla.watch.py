1b89d1f8aecb556587566a2e92ca3604a87e160a
Bastian Eicher <bastian@eicher.net>
2017-09-18 18:20:26 +0200

    Added old versions of FileZilla

--- FileZilla.watch.py
+++ FileZilla.watch.py
@@ -3,4 +3,4 @@ import re
 
 data = request.urlopen('https://sourceforge.net/projects/filezilla/files/FileZilla_Client/').read()
 matches = re.findall(r'([0-9\.]+)</a></th>\n\s*<td headers="files_date_h" class="opt"><abbr title="(....-..-..)', data.decode())
-releases = [{'version': match[0], 'released': match[1]} for match in matches]
+releases = [{'version': match[0], 'released': match[1]} for match in matches if match[0][0:4] != '3.0.']
