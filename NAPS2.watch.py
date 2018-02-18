929640ae46cf58e31f03b31be93b86801230a2bf
Bastian Eicher <bastian@eicher.net>
2018-02-18 14:58:11 +0100

    Added NAPS2

---
+++ NAPS2.watch.py
@@ -0,0 +1,5 @@
+from urllib import request
+import json
+
+data = request.urlopen('https://api.github.com/repos/cyanfish/naps2/releases').read().decode('utf-8')
+releases = [{'version': release['name'], 'released': release['published_at'][0:10]} for release in json.loads(data) if 'b' not in release['name']]
