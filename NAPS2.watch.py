ba686872c90356eb9b003da9917d6dfb55a86f4f
Bastian Eicher <bastian@eicher.net>
2018-07-04 18:40:12 +0200

    Updated NAPS2

--- NAPS2.watch.py
+++ NAPS2.watch.py
@@ -2,4 +2,4 @@ from urllib import request
 import json
 
 data = request.urlopen('https://api.github.com/repos/cyanfish/naps2/releases').read().decode('utf-8')
-releases = [{'version': release['name'], 'released': release['published_at'][0:10]} for release in json.loads(data) if 'b' not in release['name']]
+releases = [{'version': release['name'], 'released': release['published_at'][0:10]} for release in json.loads(data) if release['assets'] and 'b' not in release['name']]
