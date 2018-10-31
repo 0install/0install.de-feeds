a65420e010ca733efdd2d804d0e1c13ce7582a04
Bastian Eicher <bastian@eicher.net>
2018-10-31 16:31:21 +0100

    Updated IntelliJ IDEA

---
+++ IntelliJ_IDEA-Community.watch.py
@@ -0,0 +1,5 @@
+from urllib import request
+import json
+
+data = request.urlopen('https://data.services.jetbrains.com/products/releases?code=IIC&type=release').read().decode('utf-8')
+releases = [{'version': release['version'], 'major-version': release['majorVersion'], 'build': release['build'], 'released': release['date']} for release in json.loads(data)['IIC'] if 'downloads' in release and 'linuxWithoutJDK' in release['downloads']]
