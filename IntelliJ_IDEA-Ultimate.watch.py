3fa9671caa1212b598ae0fc797b6ebbb1989cee4
Bastian Eicher <bastian@eicher.net>
2019-07-25 17:05:25 +0200

    Updated JetBrains IDEs

--- IntelliJ_IDEA-Ultimate.watch.py
+++ IntelliJ_IDEA-Ultimate.watch.py
@@ -2,4 +2,4 @@ from urllib import request
 import json
 
 data = request.urlopen('https://data.services.jetbrains.com/products/releases?code=IIU&type=release').read().decode('utf-8')
-releases = [{'version': release['version'], 'major-version': release['majorVersion'], 'build': release['build'], 'released': release['date']} for release in json.loads(data)['IIU'] if 'downloads' in release and 'linuxWithoutJDK' in release['downloads']]
+releases = [{'version': release['version'], 'major-version': release['majorVersion'], 'build': release['build'], 'released': release['date']} for release in json.loads(data)['IIU'] if 'downloads' in release and 'linuxWithoutJBR' in release['downloads']]
