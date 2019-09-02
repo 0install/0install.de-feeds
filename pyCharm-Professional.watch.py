fd88583a4d290b03f09b44a5af7086060210626f
Bastian Eicher <bastian@eicher.net>
2019-09-02 21:09:32 +0200

    Updated pyCharm

--- pyCharm-Professional.watch.py
+++ pyCharm-Professional.watch.py
@@ -2,4 +2,4 @@ from urllib import request
 import json
 
 data = request.urlopen('https://data.services.jetbrains.com/products/releases?code=PCP&type=release').read().decode('utf-8')
-releases = [{'version': release['version'], 'major-version': release['majorVersion'], 'released': release['date']} for release in json.loads(data)['PCP'] if 'downloads' in release and release['version'].startswith('20')]
+releases = [{'version': release['version'], 'major-version': release['majorVersion'], 'released': release['date']} for release in json.loads(data)['PCP'] if 'downloads' in release and release['version'].startswith('20') and not release['version'].startswith('2016') and not release['version'].startswith('2017')]
