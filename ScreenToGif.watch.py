541600a9f2846a6f5db5c5db51930082732fba81
Bastian Eicher <bastian@eicher.net>
2020-02-08 13:10:23 +0100

    Moved some feeds to apps.0install.net

--- ScreenToGif.watch.py
+++
@@ -1,5 +0,0 @@
-from urllib import request
-import json
-
-data = request.urlopen('https://api.github.com/repos/NickeManarin/ScreenToGif/releases').read().decode('utf-8')
-releases = [{'version': release['tag_name'], 'released': release['published_at'][0:10]} for release in json.loads(data) if release['assets']]
