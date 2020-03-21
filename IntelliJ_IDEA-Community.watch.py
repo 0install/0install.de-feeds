0c8032c07ce5b29ea559a71ec71362f16a90f1ea
Bastian Eicher <bastian@eicher.net>
2020-03-21 14:44:23 +0100

    Updated JetBrains IDEs

--- IntelliJ_IDEA-Community.watch.py
+++ IntelliJ_IDEA-Community.watch.py
@@ -2,4 +2,9 @@ from urllib import request
 import json
 
 data = request.urlopen('https://data.services.jetbrains.com/products/releases?code=IIC&type=release').read().decode('utf-8')
-releases = [{'version': release['version'], 'major-version': release['majorVersion'], 'build': release['build'], 'released': release['date']} for release in json.loads(data)['IIC'] if 'downloads' in release and 'linuxWithoutJBR' in release['downloads']]
+releases = [{
+    'version': release['version'],
+    'major-version': release['majorVersion'],
+    'build': release['build'],
+    'released': release['date']
+} for release in json.loads(data)['IIC'] if 'downloads' in release and 'linuxWithoutJBR' in release['downloads']]
--- Rider.watch.py
+++ Rider.watch.py
@@ -2,4 +2,9 @@ from urllib import request
 import json
 
 data = request.urlopen('https://data.services.jetbrains.com/products/releases?code=RD&type=release').read().decode('utf-8')
-releases = [{'version': release['version'], 'download-url': release['downloads']['zip']['link'], 'major-version': release['majorVersion'], 'released': release['date']} for release in json.loads(data)['RD'] if 'downloads' in release]
+releases = [{
+    'version': release['version'],
+    'released': release['date'],
+    'download-url-windows': release['downloads']['zip']['link'],
+    'download-url-linux': release['downloads']['linux']['link']
+} for release in json.loads(data)['RD'] if 'downloads' in release]
