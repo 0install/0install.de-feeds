9be41a2fdcfa7056bb4f74696716d559685e2fb7
Bastian Eicher <bastian@eicher.net>
2020-04-30 18:16:01 +0200

    Updated JetBrains IDEs

--- Rider.watch.py
+++ Rider.watch.py
@@ -3,7 +3,7 @@ import json
 
 data = request.urlopen('https://data.services.jetbrains.com/products/releases?code=RD&type=release').read().decode('utf-8')
 releases = [{
-    'version': release['version'],
+    'version': release['version'] if release['version'].count('.') == 2 else release['version'] + '.0',
     'released': release['date'],
     'download-url-windows': release['downloads']['zip']['link'],
     'download-url-linux': release['downloads']['linux']['link']
