71b75f9ddd93b7a22c22b39635850ea0c63a94a6
Bastian Eicher <bastian@eicher.net>
2024-06-02 14:17:56 +0200

    Removed AutoIt watch script

--- AutoIt.watch.py
+++
@@ -1,6 +0,0 @@
-from urllib import request
-import re
-
-data = request.urlopen('https://www.autoitscript.com/autoit3/files/archive/autoit/').read().decode('utf-8')
-matches = re.findall(r'autoit-v([0-9\.]+).zip<\/a>\s*(....-..-..)', data)
-releases = [{'version': match[0], 'released': match[1]} for match in matches]
