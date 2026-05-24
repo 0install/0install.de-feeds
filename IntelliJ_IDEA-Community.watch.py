d2e2b4e13bcc28047001013219a015c3d2f960a5
Bastian Eicher <bastian@eicher.net>
2026-05-24 21:31:30 +0200

    Removed obsolete watch scripts

--- IntelliJ_IDEA-Community.watch.py
+++
@@ -1,9 +0,0 @@
-from urllib import request
-import json
-
-data = request.urlopen('https://data.services.jetbrains.com/products/releases?code=IIC&type=release').read().decode('utf-8')
-releases = [{
-    'version': release['version'],
-    'build': release['build'],
-    'released': release['date']
-} for release in json.loads(data)['IIC'] if 'downloads' in release and 'linuxARM64' in release['downloads']]
--- pyCharm-Community.watch.py
+++
@@ -1,8 +0,0 @@
-from urllib import request
-import json
-
-data = request.urlopen('https://data.services.jetbrains.com/products/releases?code=PCC&type=release').read().decode('utf-8')
-releases = [{
-    'version': release['version'],
-    'released': release['date']
-} for release in json.loads(data)['PCC'] if 'downloads' in release and 'windowsZipARM64' in release['downloads']]
